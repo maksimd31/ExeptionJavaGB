2231d190952db85bf654f17bc6ee357954da405c
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-04-16 12:28:09 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try (Counter counter = new Counter()) {
+            counter.add();
+            counter.add();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
     }
+    public static class Counter implements AutoCloseable {
+        private int count;
+        private boolean isClosed;
+
+        public Counter() {
+            this.count = 0;
+            this.isClosed = false;
+        }
+
+        public void add() {
+            if(isClosed){
+                throw new IllegalStateException("Счетчик закрыт");
+            }
+            count++;
+        }
+
+        @Override
+        public void close() throws Exception {
+            if(!isClosed){
+                isClosed = true;
+            }else {
+                throw new IllegalStateException("Счетчик уже закрыт");
+            }
+        }
+    }
+
 }
